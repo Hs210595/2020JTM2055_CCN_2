5b812e226f6ad8ce06002906e598cf09fe39b3db
Hs210595 <hrishikeshsaste@gmail.com>
2020-12-02 14:39:30 +0530

    My code

--- ps1_a.py
+++ ps1_a.py
@@ -3,28 +3,43 @@ import random
 import threading
 arrival_times=[]
 next_time=0
+next_ser_time=0
 rateParameter=10
-def arrive:
-    print('Arrived')
+service_times=[]
+serviceRate=15
+i=0
+n=0
+def arrive():
+    global n
+    n=n+1
+    print('{0} Arrived'.format(n))
+    if(n==1):
+        start_ser()
 for i in range(30):
     time= -math.log(1.0 - random.random()) / rateParameter
     next_time=next_time+time
     arrival_times.append(next_time)
+def dep():
+    global n
+    print('{0} departed'.format(0))
+
+for i in range(30):
+    time_s= -math.log(1.0 - random.random()) / serviceRate
+    next_ser_time=next_ser_time+time
+    service_times.append(next_time)
 print(arrival_times)
-time1=threading.Timer(arrival_times[i],arrive)
-time1=threading.Timer(arrival_times[i+1],arrive)
-time1=threading.Timer(arrival_times[i+2],arrive)
-time1=threading.Timer(arrival_times[i+3],arrive)
-time1=threading.Timer(arrival_times[i+4],arrive)
-time1=threading.Timer(arrival_times[i+5],arrive)
-time1=threading.Timer(arrival_times[i+6],arrive)
-time1=threading.Timer(arrival_times[i+7],arrive)
-time1=threading.Timer(arrival_times[i+8],arrive)
-time1=threading.Timer(arrival_times[i+9],arrive)
-time1=threading.Timer(arrival_times[i+10],arrive)
-time1=threading.Timer(arrival_times[i+11],arrive)
-time1=threading.Timer(arrival_times[i+12],arrive)
-time1=threading.Timer(arrival_times[i+13],arrive)
-time1=threading.Timer(arrival_times[i+14],arrive)
+print(service_times)
+timer1=threading.Timer(arrival_times[i],arrive)
+timer1.start()
+timer2=threading.Timer(arrival_times[1],arrive)
+timer2.start()
+timer3=threading.Timer(arrival_times[28],arrive)
+timer3.start()
+def start_ser():
+    t1=threading.Timer(service_times[0],dep)
+    t1.start()
+    t2=threading.Timer(service_times[1],dep)
+    t2.start()
+    t3=threading.Timer(service_times[3],dep)
 
 
